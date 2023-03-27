b6bb5219880b860513d44f5cf21b7c528f3c1c45
Mitziann2520 <129090048+Mitziann2520@users.noreply.github.com>
2023-03-27 23:43:58 +0800

    Add files via upload

---
+++ userpass.py
@@ -0,0 +1,24 @@
+attempts=True
+
+while attempts==True:
+
+    username=input("Enter username: \n")
+
+    password=input("Enter password:\n")
+
+    if username=="mitzi"and password=="almario":
+
+        print("sakto imo user kag password")
+
+        attempts=False
+
+       
+
+    else:
+
+        print("may sala sa user kag pass mo ya")
+
+        
+
+            
+
